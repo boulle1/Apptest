10fa6c48d9f503456e3473ebc9457bc2d06e4bd8
Margaux <margaux.boulle@laposte.net>
2019-02-01 14:58:32 +0000

    test
    Signed-off-by: Margaux <margaux.boulle@laposte.net>

--- app/src/test/java/com/example/apptest/ExampleUnitTest.java
+++ app/src/test/java/com/example/apptest/ExampleUnitTest.java
@@ -1,6 +1,9 @@
 package com.example.apptest;
 
-import org.junit.Test;
+
+import org.junit.*;
+
+
 
 import static org.junit.Assert.*;
 
@@ -10,8 +13,23 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
+    int totalNumberOfApplicants = 0;
+    int totalNumberOfAcceptableApplicants = 10;
+
+    @Before
+    public void setData(){
+        this.totalNumberOfApplicants = 9;
+    }
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testAssertions() {
+        assertTrue((this.totalNumberOfApplicants != this.totalNumberOfAcceptableApplicants));
     }
 }
